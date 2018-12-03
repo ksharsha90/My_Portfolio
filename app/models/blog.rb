d889c42dc5a84bbd6f4edac30bdaaba27d141de8
lallhar <lfc5timesktt@gmail.com>
2018-12-03 09:49:32 +0530

    enum status added, drat and published

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
-
-	 extend FriendlyId
- 	 friendly_id :title, use: :slugged
+	enum status: { draft: 0, published: 1}  
+	extend FriendlyId
+ 	friendly_id :title, use: :slugged
 	
 end
