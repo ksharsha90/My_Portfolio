aa1e2744bc61ed4aa5776f437800a863d7765b53
ksharsha90 <ksharsha90@gmail.com>
2018-12-21 11:10:50 +0530

    small changes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :destroy, :show]
   layout 'portfolio'
   	
 	def index
@@ -23,12 +24,9 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
-
     	respond_to do |format|
      		 if @portfolio_item.update(portfolio_params)
         		format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }        		
@@ -39,13 +37,11 @@ class PortfoliosController < ApplicationController
   	end
 
   	def show
-  		@portfolio_item = Portfolio.find(params[:id])
   	end
 
   	def destroy
   		#perform the lookup
-  		@portfolio_item = Portfolio.find(params[:id])
-
+  		
   		#Destroy, Delete the Record
   		@portfolio_item.destroy
 
@@ -63,5 +59,9 @@ class PortfoliosController < ApplicationController
   										  :body,
 										   technologies_attributes: [:name])
   	end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,6 +4,14 @@ class Blog < ApplicationRecord
  	friendly_id :title, use: :slugged
 
  	validates_presence_of :title, :body
+
+ 	def self.special_blogs
+ 		all
+ 	end
+
+ 	def self.featured_blogs
+ 		limit(2)
+ 	end
 	
 
 end
