079e4e537a0330b9bd23d11c161979579d0d8a22
ksharsha90 <ksharsha90@gmail.com>
2018-12-21 12:01:33 +0530

    save appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,8 @@ class ApplicationController < ActionController::Base
 	include CurrentUserConcern
 	include DefaultPageContent
 
-	before_action :copyright_generator
+	
 
-	def copyright_generator
-    	GurujadaViewTool::Renderer.copyright 'Harsha', 'All rights reserved'
-    end
+	
 end
 
